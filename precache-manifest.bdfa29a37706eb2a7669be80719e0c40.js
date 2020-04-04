acdd2c18e96af336fe16134cc8368e8821f9c2c7
Yegor <egor.muhladi@gmal.com>
2020-04-04 20:04:30 +0300

    Updates

---
+++ precache-manifest.bdfa29a37706eb2a7669be80719e0c40.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61da0f75fe69fa369e088b2400bd877b",
+    "url": "/centralvac/index.html"
+  },
+  {
+    "revision": "3c7b32abc850f9a0b7c7",
+    "url": "/centralvac/static/css/2.b0432c10.chunk.css"
+  },
+  {
+    "revision": "7cddaeea85e76611b737",
+    "url": "/centralvac/static/css/main.c37d755d.chunk.css"
+  },
+  {
+    "revision": "3c7b32abc850f9a0b7c7",
+    "url": "/centralvac/static/js/2.f2807f2c.chunk.js"
+  },
+  {
+    "revision": "ab5668d692b255e17653acfd25f1138b",
+    "url": "/centralvac/static/js/2.f2807f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cddaeea85e76611b737",
+    "url": "/centralvac/static/js/main.b36e9d82.chunk.js"
+  },
+  {
+    "revision": "20943a1cb317c30788e7",
+    "url": "/centralvac/static/js/runtime-main.ecfd7cbf.js"
+  },
+  {
+    "revision": "cdf15801731b8d577f21415237e73820",
+    "url": "/centralvac/static/media/house.cdf15801.png"
+  },
+  {
+    "revision": "bede0ddbc0eaec8fc3effa616c7c4b8e",
+    "url": "/centralvac/static/media/house100.bede0ddb.jpg"
+  },
+  {
+    "revision": "4c83f3bbbe338c6a4d1ad6dc52df79a9",
+    "url": "/centralvac/static/media/house250.4c83f3bb.jpg"
+  },
+  {
+    "revision": "961e17f12c75c74d81621f791080e8f1",
+    "url": "/centralvac/static/media/house350.961e17f1.jpg"
+  },
+  {
+    "revision": "cc38eb524deb07282f082dc3e5609928",
+    "url": "/centralvac/static/media/house500.cc38eb52.jpg"
+  },
+  {
+    "revision": "6e9820823b6855a38bd3344807e22785",
+    "url": "/centralvac/static/media/house70.6e982082.jpg"
+  },
+  {
+    "revision": "62ed46ef609c19252b05569d5f789166",
+    "url": "/centralvac/static/media/logo.62ed46ef.jpg"
+  },
+  {
+    "revision": "6eac84e4fc1238527656ad9337ecda02",
+    "url": "/centralvac/static/media/place.6eac84e4.jpg"
+  },
+  {
+    "revision": "0745dde41147421008a99cd6ecdc9400",
+    "url": "/centralvac/static/media/service_img.0745dde4.jpg"
+  },
+  {
+    "revision": "3d0ef04971f20d274beca962ea89c354",
+    "url": "/centralvac/static/media/slide1.3d0ef049.png"
+  },
+  {
+    "revision": "03696543668509d8968e7a131329129a",
+    "url": "/centralvac/static/media/slide1_pic.03696543.png"
+  },
+  {
+    "revision": "c6df595480fa1dd6c38d4134551b27cc",
+    "url": "/centralvac/static/media/slide2.c6df5954.png"
+  },
+  {
+    "revision": "89b9668f6b9de75d0f069c3ce8be5eb1",
+    "url": "/centralvac/static/media/slide2_pic.89b9668f.png"
+  }
+]);
